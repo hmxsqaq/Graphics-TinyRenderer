57cb11044216a35178d363aa85e0136a598cdd6d
hmxs <hmxsqaq@163.com>
2024-05-24 01:26:52 +0800

    Update barycentric

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is the final project for Computer Graphics course, a tiny software renderer
 
 It does not rely on any third-party libraries, which can be a good practice for learning computer graphics.
 
-## Line drawing
+## Line Drawing
 
 Implement of Bresenham's line algorithm
 
@@ -48,8 +48,68 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 ```
 
-## Model loading
+## Model Loading
 
 [.obj](http://en.wikipedia.org/wiki/Wavefront_.obj_file) is a geometry definition file format which is simple and open.
 
-[model.h](model.h) defines the `Model` class, which can read [.obj](http://en.wikipedia.org/wiki/Wavefront_.obj_file) model files and store the vertices and faces in it.
+[model.h](model.h) defines the `Model` class, which can read `.obj` model files and store the vertices and faces in it.
+
+## Triangle Drawing
+
+### Line Sweeping
+
+A traditional way to draw a triangle, but it is an old-school approach designed for mono-thread CPU programming, which is not used so much.
+
+```c++
+// line sweeping triangle drawing
+void triangle(Vec2i p0, Vec2i p1, Vec2i p2, TGAImage &image, const TGAColor& color) {
+    if (p0.y == p1.y && p1.y == p2.y) return;
+    // make p0.y < p1.y < p2.y
+    if (p0.y > p1.y) std::swap(p0, p1);
+    if (p0.y > p2.y) std::swap(p0, p2);
+    if (p1.y > p2.y) std::swap(p1, p2);
+
+    int total_height = p2.y - p0.y;
+    for (int y = 0; y <= total_height; ++y) {
+        bool second_half = y > p1.y - p0.y || p0.y == p1.y;
+        int segment_height = second_half ? p2.y - p1.y : p1.y - p0.y;
+        float alpha = (float)y / (float)total_height;
+        float beta = (float)(y - (second_half ? p1.y - p0.y : 0)) / (float) segment_height;
+        Vec2i A = p0 + (p2 - p0) * alpha;
+        Vec2i B = second_half ? p1 + (p2 - p1) * beta : p0 + (p1 - p0) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int x = A.x; x <= B.x; ++x) {
+            image.set(x, p0.y + y, color);
+        }
+    }
+}
+```
+
+### Rasterization
+
+Iterate through all pixels of a bounding box and check whether a point belongs a 2D triangle.
+
+There are two ways to check whether a point belongs a 2D triangle.
+
+- Barycentric
+
+[Barycentric](https://en.wikipedia.org/wiki/Barycentric_coordinate_system) is truly an useful feature of triangle which can be used to interpolation. Here, we just use it to check a point.
+
+```c++
+// get barycentric
+Vec3f barycentric(Vec2i *pts, Vec2i P) {
+    Vec3i x(pts[1].x - pts[0].x, pts[2].x - pts[0].x, pts[0].x - P.x);
+    Vec3i y(pts[1].y - pts[0].y, pts[2].y - pts[0].y, pts[0].y - P.y);
+    Vec3i u = x ^ y;
+
+    // u.z is Int, so "abs(u.z) < 1" means "u.z == 0"
+    // if u.z is 0, then triangle is degenerate
+    if (std::abs(u.z) < 1) return {-1, 1, 1};
+
+    return {1.0f - (float)(u.x + u.y) / (float)u.z,
+            (float)u.x / (float)u.z,
+            (float)u.y / (float)u.z};
+}
+```
+
+- Cross Product
--- main.cpp
+++ main.cpp
@@ -1,73 +1,108 @@
 #include "tgaimage.h"
 #include "model.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor white =  TGAColor(255,   255,    255,    255);
+const TGAColor red =    TGAColor(255,   0,      0,      255);
+const TGAColor green =  TGAColor(0,     255,    0,      255);
 
 Model *model = nullptr;
-const int width  = 800;
-const int height = 800;
+const int width  = 200;
+const int height = 200;
 
 // Bresenham's line algorithm
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, const TGAColor& color) {
     // if (dx < dy) the line is steep
     // we need to sample it along y axis
     // but we can also transpose it, then we can still use x axis
     // just remember to re-transpose it when drawing it
     bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        // transpose
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (x0 > x1) { // make it left−to−right
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+
+    // make it left−to−right
+    if (p0.x > p1.x) {
+        std::swap(p0, p1);
     }
 
     // use error to approximate the distance between the line and the pixel, which can improve performance
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    int derror = std::abs(dy) * 2;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
+    int per_error = std::abs(dy) * 2;
     int error = 0;
-    int y = y0;
+    int y = p0.y;
 
-    for (int x = x0; x <= x1; x++) {
+    for (int x = p0.x; x <= p1.x; x++) {
         if (steep) {
             image.set(y, x, color); // if transposed, de−transpose
         } else {
             image.set(x, y, color);
         }
-        error += derror;
+        error += per_error;
         if (error > dx) {
-            y += (y1 > y0 ? 1 : -1);
+            y += (p1.y > p0.y ? 1 : -1);
             error -= 2 * dx;
         }
     }
 }
 
-int main(int argc, char** argv) {
-    if (argc == 2) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("../obj/african_head.obj");
-    }
+// get barycentric
+Vec3f barycentric(Vec2i *pts, Vec2i P) {
+    Vec3i x(pts[1].x - pts[0].x, pts[2].x - pts[0].x, pts[0].x - P.x);
+    Vec3i y(pts[1].y - pts[0].y, pts[2].y - pts[0].y, pts[0].y - P.y);
+    Vec3i u = x ^ y;
 
-    TGAImage image(width, height, TGAImage::RGB);
-    for (int i = 0; i < model->facesSize(); ++i) {
-        std::vector<int> face = model->face(i);
-        for (int j = 0; j < 3; ++j) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            int x0 = (v0.x + 1.0f) * width / 2.0f;
-            int y0 = (v0.y + 1.0f) * height / 2.0f;
-            int x1 = (v1.x + 1.0f) * width / 2.0f;
-            int y1 = (v1.y + 1.0f) * height / 2.0f;
-            line(x0, y0, x1, y1, image, white);
+    // u.z is Int, so "abs(u.z) < 1" means "u.z == 0"
+    // if u.z is 0, then triangle is degenerate
+    if (std::abs(u.z) < 1) return {-1, 1, 1};
+
+    return {1.0f - (float)(u.x + u.y) / (float)u.z,
+            (float)u.x / (float)u.z,
+            (float)u.y / (float)u.z};
+}
+
+// line sweeping triangle drawing
+void triangle(Vec2i p0, Vec2i p1, Vec2i p2, TGAImage &image, const TGAColor& color) {
+    if (p0.y == p1.y && p1.y == p2.y) return;
+    // make p0.y < p1.y < p2.y
+    if (p0.y > p1.y) std::swap(p0, p1);
+    if (p0.y > p2.y) std::swap(p0, p2);
+    if (p1.y > p2.y) std::swap(p1, p2);
+
+    int total_height = p2.y - p0.y;
+    for (int y = 0; y <= total_height; ++y) {
+        bool second_half = y > p1.y - p0.y || p0.y == p1.y;
+        int segment_height = second_half ? p2.y - p1.y : p1.y - p0.y;
+        float alpha = (float)y / (float)total_height;
+        float beta = (float)(y - (second_half ? p1.y - p0.y : 0)) / (float) segment_height;
+        Vec2i A = p0 + (p2 - p0) * alpha;
+        Vec2i B = second_half ? p1 + (p2 - p1) * beta : p0 + (p1 - p0) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int x = A.x; x <= B.x; ++x) {
+            image.set(x, p0.y + y, color);
         }
     }
+}
+
+int main(int argc, char** argv) {
+//    if (argc == 2) {
+//        model = new Model(argv[1]);
+//    } else {
+//        model = new Model("../obj/african_head.obj");
+//    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
     image.flip_vertically();
-    image.write_tga_file("../image/african_head_line.tga");
+    image.write_tga_file("../image/output.tga");
     return 0;
 }
